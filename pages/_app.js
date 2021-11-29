532840b9910d21e66bf5049ee8ac1e45dce8ccc5
Taylor Zanke <taylor.zanke@gmail.com>
2021-11-28 17:36:37 -0800

    add framer motion page ease in

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,8 @@
 import Head from 'next/head'
-import { useRouter } from 'next/router'
 import { ThemeProvider } from 'styled-components'
 import { GlobalStyle, theme } from '../styles/theme'
+import { useRouter } from 'next/router'
+import { AnimatePresence, motion } from "framer-motion"
 
 import {
   Layout
@@ -13,15 +14,25 @@ function App({ Component, pageProps }) {
   return (
     <ThemeProvider theme={theme}>
 
-      <GlobalStyle />
-
       <Head>
         <meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0,user-scalable=0" />
       </Head>
 
-      <Layout>
-        <Component key={router.pathname} {...pageProps} />
-      </Layout>
+      <GlobalStyle />
+
+      <AnimatePresence exitBeforeEnter>
+          <motion.div
+            initial={{opacity: 0}}
+            animate={{opacity: 1}}
+            exit={{opacity: 0}}
+            transition={{duration: 2}}
+          >
+            <Layout>
+              <Component key={router.pathname} {...pageProps} />
+            </Layout>
+          </motion.div>
+        </AnimatePresence>
+
 
     </ThemeProvider>
   )
